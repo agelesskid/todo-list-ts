78c5b17f004556272408f3be4ac8adf745825d53
ageless <agelesskidxd@gmail.com>
2023-04-05 17:05:10 +0200

    added localStorage functionality for todo-list

--- src/task.ts
+++ src/task.ts
@@ -10,7 +10,8 @@ type Task = {
 const list = document.querySelector<HTMLUListElement>('#todo-list')
 const form = document.querySelector<HTMLFormElement>('#todo-form')
 const input = document.querySelector<HTMLInputElement>('#todo-input')
-const tasks: Task[] = []
+const tasks: Task[] = loadTasks()
+tasks.forEach(addListItem)
 
 form?.addEventListener("submit", e => {
   e.preventDefault()
@@ -26,6 +27,7 @@ form?.addEventListener("submit", e => {
 
   addListItem(newTask)
   tasks.push(newTask)
+  saveTasks()
   input.value = ''
 })
 
@@ -35,10 +37,22 @@ function addListItem(task: Task){
   const checkbox = document.createElement('input')
   checkbox.addEventListener('change', () => {
     task.completed = checkbox.checked
+    saveTasks()
   })
   checkbox.type = 'checkbox'
   checkbox.checked = task.completed
   label.append(checkbox, task.title)
   item.append(label)
   list?.append(item)
+  
+}
+
+function saveTasks() {
+    localStorage.setItem('TASKS', JSON.stringify(tasks))
+}
+
+function loadTasks(): Task[] {
+    const taskJSON = localStorage.getItem("TASKS")
+    if (taskJSON == null) return []
+    return JSON.parse(taskJSON)
 }
